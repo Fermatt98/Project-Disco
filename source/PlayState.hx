ab08d4fb5b5e3a1fb0a4c0e3c0afa7b997a5892b
gonzaloElMasPiola <zeiss.gonzalo@cts.edu.ar>
2016-09-22 17:42:51 -0700

    aguanta

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,9 +9,26 @@ import flixel.math.FlxMath;
 
 class PlayState extends FlxState
 {
+	//-------Ecenario-------//
+	private var piso:FlxSprite;
+	private var paredA:FlxSprite;
+	private var paredB:FlxSprite;
+	private var techo:FlxSprite;
+	//-------Armas------//
+	private var TrampaDisco:FlxSprite;
 	override public function create():Void
 	{
 		super.create();
+		piso = new Piso(0, 580);
+		paredA = new Pared();
+		paredB = new Pared(780, 0);
+		techo = new Techo();
+		TrampaDisco = new CajaDisco(50,50,4);
+		add(TrampaDisco);
+		add(piso);
+		add(paredA);
+		add(paredB);
+		add(techo);
 	}
 
 	override public function update(elapsed:Float):Void
